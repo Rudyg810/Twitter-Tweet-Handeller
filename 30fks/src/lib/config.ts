58244133f4ce3b710acb65057c655d382d0fda5a
rudyg810 <rudyg810@gmail.com>
2024-06-01 23:06:04 +0530

    new

--- 30fks/src/lib/config.ts
+++ 30fks/src/lib/config.ts
@@ -1,20 +1,6 @@
 const config = {
-    googleClientId:  "997377239835-hd1kkfnci9e4ekoi64mf882n0jde33qk.apps.googleusercontent.com",
-    googleClientSecret:'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',
-    googleCallbackURL: 'http://localhost:3000',
-    StripeSecretKey:"sk_test_51Omv5MSF8iNIRdD3b8He56DG4gge2S48h0sgHKudOzFZcEh1bZLBgmUm6ynWC3lOutfrJYznDPDQ5J2bOjZh8j1800gUAOFFtU",
-    StripePublishableKey:"pk_test_51Omv5MSF8iNIRdD3SDTGHXaHGKCahSBDCMPUDsq3QqPqfgQy2pNutxbHK5JrYFOOma2YddzdkfendTt98gug3OGX00FOhykQzw",
-    port:"http://localhost:8080",
-    flaskport:"http://127.0.0.1:5000",
-    apiKey:"AIzaSyB1MTImxad1l7YaUtIlVFToGR_kOjpNRl4",
-    X_API_KEY:"RvMQtRzyYiYfuGoZmYNEZsXxO",
-    X_API_SECRET:"JC4OZa4CbRM9EdSx5z0J6enfx4AwY6JrQdD86G1vWf1IHNzTZY",
-    X_CLIENT_TOKEN:"QkZWMEFYS3phbEV4QzVHaHo4d1A6MTpjaQ",
-    X_CLIENT_SECRET:"c85D5zY-M1VVlbnjulmqKQEbtKpU_cNc7C_7ZC_K5X5FmaxkUK",
-    X_ACESS_TOKEN:"1546014827315068929-RmIgU6yqtHhpQp9b6wx3X5bPCv39LU",
-    X_ACESS_SECRET:"8l1qEtNc8T7OqEdIcMtvbtkQz9Ac8ixL7V3teEti91aK6",
-    X_BEARER_TOKEN:"AAAAAAAAAAAAAAAAAAAAAFJltQEAAAAASUb2pkGJ4mmekYaZvQGwjRHYOeI%3Da03dvFC6PsX7Uk8Q1vJQfPUqKx7EKEe1Tf62PbDVobRW4Slkmz",
-    X_APP_ID:"1546014827315068929"
+    port:"http://localhost:8000",
+
   };
 
   export default config;
